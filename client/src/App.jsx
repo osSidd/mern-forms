7ef992e162b4163f99db9ff82bb3c9cd2a04c3b4
Osama <107386980+osSidd@users.noreply.github.com>
2024-01-16 21:40:04 +0530

    formbuilder - homepage - temp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,15 @@ import { Suspense, lazy } from 'react'
 
 const FormBuilder = lazy(() => import('./pages/formBuilder'))
 const FormRender = lazy(() => import('./pages/formRender'))
-
+// const homePageBtn = 'px-8 py-3 bg-blue-400 text-white font-bold mx-5 mb-4 block w-fit'
 export default function App(){
   return(
     <div className='min-h-screen bg-teal-800 '>
       <BrowserRouter>
         <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/' element={<div><Link to='/builder'>Builder</Link><Link to='/render'>Render</Link></div>}/>
-          <Route path='/builder' element={<FormBuilder/>}/>
+          {/* <Route path='/' element={<div><Link className={homePageBtn} to='/builder'>Builder</Link><Link className={homePageBtn} to='/render'>Render</Link></div>}/> */}
+          <Route path='/' element={<FormBuilder/>}/>
           <Route path='render' element={<FormRender/>}/>
         </Routes>
         </Suspense>
