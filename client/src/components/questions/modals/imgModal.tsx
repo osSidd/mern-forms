49e656d8d76683e6b2a6c1138659de58be353c7d
Osama <107386980+osSidd@users.noreply.github.com>
2024-05-16 22:03:45 +0530

    image modal responsive

--- client/src/components/questions/modals/imgModal.tsx
+++ client/src/components/questions/modals/imgModal.tsx
@@ -3,7 +3,7 @@ import Icon from "../icon";
 
 export default function ImgModal({modal, showModal}: {modal: {display: boolean}, showModal: (display: boolean) => void}){
     return (
-        <div className={`${modal.display ? 'block' : 'hidden'} fixed top-14 bottom-14 left-32 right-32 p-4 z-50 bg-white shadow-2xl`}>
+        <div className={`${modal.display ? 'block' : 'hidden'} fixed top-24 md:top-48 lg:top-14 bottom-24 md:bottom-48 lg:bottom-14 left-8 md:left-32 right-8 md:right-32 p-4 z-50 bg-white shadow-2xl`}>
             <div onClick={e => showModal(false)} className="ml-auto mr-4 w-fit"><Icon icon='times' size="2xl"/></div>
         </div>
     )
